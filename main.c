bdac0d7283a29c52203331aca42c29b3b79f149a
Denton Liu <liu.denton@gmail.com>
2017-08-14 03:46:44 -0400

    Initial commit

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <bluetooth/bluetooth.h>
+#include <bluetooth/rfcomm.h>
+#include <getopt.h>
+#include <stddef.h>
+
+int main(int argc, char[] *argv) {
+// TODO: implement command arg parsing
+//	struct option command_options[] = {
+//		{ "set-name", no_argument, NULL, 1 }
+//	};
+
+	return 0;
+}
