993fc091abb4474888d3c79eb529d69fe18bc04f
Denton Liu <liu.denton@gmail.com>
2017-08-23 06:21:07 -0400

    Add options for adjusting noise cancelling level

--- based.c
+++ based.c
@@ -6,6 +6,7 @@
 
 #define BYTE_MAX 0xFF
 #define CN_BASE_PACK_LEN 3
+#define NC_PACK_LEN 5
 
 ssize_t set_name(int sock, const char *name) {
 	char packet[CN_BASE_PACK_LEN + BYTE_MAX] = { 0x01, 0x02, 0x02 };
@@ -23,4 +24,9 @@ ssize_t set_name(int sock, const char *name) {
 	strncpy(&packet[CN_BASE_PACK_LEN + 1], name, BYTE_MAX);
 
 	return write(sock, packet, packet_len);
+}
+
+ssize_t noise_cancelling(int sock, char level) {
+	char packet[NC_PACK_LEN] = { 0x01, 0x06, 0x02, 0x01, level };
+	return write(sock, packet, NC_PACK_LEN);
 }
