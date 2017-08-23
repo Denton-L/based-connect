a263f0ecd3463db8735a1686f74f73d9ef0a04b9
Denton Liu <liu.denton@gmail.com>
2017-08-23 05:48:30 -0400

    Add command parsing for main

--- main.c
+++ main.c
@@ -1,33 +1,72 @@
 #include <bluetooth/bluetooth.h>
 #include <bluetooth/rfcomm.h>
+#include <errno.h>
 #include <getopt.h>
-#include <sys/socket.h>
+#include <stdlib.h>
 #include <unistd.h>
 
 #include "based.h"
 
-// TODO: make this dynamically read from commandline
-static char dest[18] = "04:52:C7:5B:8B:47";
-
 int main(int argc, char *argv[]) {
-// TODO: implement command arg parsing
-//	struct option command_options[] = {
-//		{ "set-name", no_argument, NULL, 1 }
-//	};
+	const char *short_opt = "+hn:";
+	const struct option long_opt[] = {
+		{ "help", no_argument, NULL, 'h' },
+		{ "set-name", required_argument, NULL, 'n' },
+		{ 0, 0, 0, 0 }
+	};
+	int opt_index = 0;
+	int opt;
 
-	int status;
+	char *change_name_arg = NULL;
+
 	int sock = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
 	struct sockaddr_rc address = {
 		AF_BLUETOOTH,
 		*BDADDR_ANY,
 		BT_CHANNEL
 	};
-	str2ba(dest, &address.rc_bdaddr);
 
-	status = connect(sock, (struct sockaddr *) &address, sizeof(address));
+	while ((opt = getopt_long(argc, argv, short_opt, long_opt, &opt_index)) != -1) {
+		switch (opt) {
+			case 'h':
+				// TODO: print usage string
+				return 0;
 
-	change_name(sock, "this is a test");
+			case 'n':
+				change_name_arg = optarg;
+				break;
+
+			case '?':
+				// TODO: print error message if opterr != 0
+				return 1;
+
+			default:
+				abort();
+		}
+	}
+
+	if (argc - 1 != optind) {
+		printf("The address must be provided as the final argument.\n");
+		return 1;
+	}
+
+	str2ba(argv[optind], &address.rc_bdaddr);
+	if (connect(sock, (struct sockaddr *) &address, sizeof(address)) != 0) {
+		printf("Could not connect to Bluetooth device. (%d)\n", errno);
+		return 1;
+	}
+
+	if (change_name_arg) {
+		if (change_name(sock, change_name_arg) < 0) {
+			printf("Failed to change name. (%d)\n", errno);
+			goto error;
+		}
+	}
 
 	close(sock);
 	return 0;
+
+error:
+	close(sock);
+	return 1;
 }
