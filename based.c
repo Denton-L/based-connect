3f90bc4961649a0e9bfdebf3ba790620df9d3688
Denton Liu <liu.denton@gmail.com>
2017-08-23 08:44:46 -0400

    Remove unnecessary variable

--- based.c
+++ based.c
@@ -12,19 +12,16 @@
 ssize_t set_name(int sock, const char *name) {
 	char packet[CN_BASE_PACK_LEN + BYTE_MAX] = { 0x01, 0x02, 0x02 };
 	size_t length = strlen(name);
-	size_t packet_len;
 
 	if (length > BYTE_MAX) {
 		length = BYTE_MAX;
 		printf("Length of name too long. Truncating to %d characters.\n", BYTE_MAX);
 	}
 
-	packet_len = CN_BASE_PACK_LEN + 1 + length;
-
 	packet[CN_BASE_PACK_LEN] = (char) length;
 	strncpy(&packet[CN_BASE_PACK_LEN + 1], name, BYTE_MAX);
 
-	return write(sock, packet, packet_len);
+	return write(sock, packet, CN_BASE_PACK_LEN + 1 + length);
 }
 
 ssize_t noise_cancelling(int sock, char level) {
